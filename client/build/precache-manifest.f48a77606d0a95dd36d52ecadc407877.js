d3add5a961c3bb70ef4e0100ae1918272ea6b571
morcos daniel <mdanielibra@gmail.com>
2020-06-18 12:53:58 +0200

    5th commit

---
+++ client/build/precache-manifest.f48a77606d0a95dd36d52ecadc407877.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "184e35f696adc5290c3b3d693e7dbe4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d3d92737b4b746eff42",
+    "url": "/static/css/main.f59d72dc.chunk.css"
+  },
+  {
+    "revision": "2b3fdd9b07e975664305",
+    "url": "/static/js/2.ff1afe8d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ff1afe8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d3d92737b4b746eff42",
+    "url": "/static/js/main.3db25848.chunk.js"
+  },
+  {
+    "revision": "ae38beac17d05289b133",
+    "url": "/static/js/runtime-main.bca95502.js"
+  },
+  {
+    "revision": "5ac47c0609ef046b1767357422a8be81",
+    "url": "/static/media/st-george.5ac47c06.jpg"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import express from 'express';
 const app = express();
+app.use(express.static(path.join(__dirname, '/../client/build')));
+
 app.get('/', async (req, res) => {
     const msg = await `Welcome to Node.js & `;
     res.status(200).json({ message: msg });
+});
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../client/build', 'index.html'));
+    // res.send('success booking church')
 });
 app.listen(process.env.PORT || 3000, () => console.log("Listening to port 3000"));
