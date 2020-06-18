9103396ba9f32c790e09863dc17ed49a9915b25b
morcos daniel <mdanielibra@gmail.com>
2020-06-18 15:08:55 +0200

    aaa

--- client/src/Pages/App/index.js
+++ client/src/Pages/App/index.js
@@ -23,6 +23,7 @@ function App({ action, loadingPage, response, getMetaData }) {
     <Router>
       <Header />
       <Layout>
+        asdasd
         {action.needed ? <Popup {...action} /> : null}
         {loadingPage ? <LoadingPage /> : null}
         {response.code ? <InfoBar items={[response.status]} classes={sty.sidePage} /> : null}
