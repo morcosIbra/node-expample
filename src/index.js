0a19ca54437602eb6cc9444f83feeaa052b2e036
morcos daniel <mdanielibra@gmail.com>
2020-06-18 12:36:43 +0200

    2nd commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-app.get('/', async (req, res) => {
-    await res.status(200).json({ message: `Welcome to Node.js & Express` });
+app.get('/', (req, res) => {
+    res.status(200).json({ message: `Welcome to Node.js & Express` });
 });
 app.listen(process.env.PORT || 3000, () => console.log("Listening to port 3000"));
