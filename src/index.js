ed23f9e21a5b7b5ac6570680a7b74cc6e5aae241
morcos daniel <mdanielibra@gmail.com>
2020-06-18 12:42:39 +0200

    4th commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
 app.get('/', async (req, res) => {
-    const msg = await `Welcome to Node.js & Express`;
+    const msg = await `Welcome to Node.js & `;
     res.status(200).json({ message: msg });
 });
 app.listen(process.env.PORT || 3000, () => console.log("Listening to port 3000"));
